2696345dc1606192907e5c1d7760e0beffd613d2
Heryan Djaruma <heryandjaruma@gmail.com>
2024-07-21 23:06:26 +0700

    jarvis alpha working prototype

--- jarvis_alpha.py
+++ jarvis_alpha.py
@@ -1,7 +1,8 @@
 import json
 import os
 import time
-from typing import Optional
+from typing import Optional, Union, Literal
+from urllib.parse import quote
 
 import requests
 from openai import OpenAI
@@ -9,9 +10,11 @@ from pydantic import BaseModel
 
 from response import ResponseT, Code, Status
 
+
 class Location(BaseModel):
-    latitude: int
-    longitude: int
+    latitude: float
+    longitude: float
+
 
 class JarvisAlphaRequest(BaseModel):
     """
@@ -25,55 +28,7 @@ class JarvisAlphaRequest(BaseModel):
 
 class JarvisAlphaService:
     BASE_PATH = '/jarvis_alpha'
-
-    ASSISTANT_ID = 'asst_xZJPXwGx0WXa6YO9N36tlmqk'  # Jarvis Alpha v2 🤖
-    # INSTRUCTIONS = ('You are Jarvis Alpha, an artificial intelligence specifically'
-    #                 'made for Grab, a super-app for ride-hailing, food delivery, '
-    #                 'and digital payment services on mobile devices that operates in Indonesia.'
-    #                 'You have given an ability to communicate with Google Maps backend, so you'
-    #                 'can construct the best response according to users need. Be helpful!')
-    INSTRUCTIONS = "You are a helpful assitant whose task is to "
-
-    TOOLS = [
-        {
-            "type": "function",
-            "function": {
-                "name": "get_places_from_text_search",
-                "description": "Get list of places from Google Maps API based on given text",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "query": {
-                            "type": "string",
-                            "description": "Given text to search with Google Maps Text Search API"
-                        }
-                    },
-                    "required": ["query"]
-                }
-            }
-        },
-        {
-            "type": "function",
-            "function": {
-                "name": "get_nearby_places",
-                "description": "Get nearby places given an exact coordinate",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "latitude": {
-                            "type": "integer",
-                            "description": "Latitude value"
-                        },
-                        "longitude": {
-                            "type": "integer",
-                            "description": "Longitude of a point"
-                        }
-                    },
-                    "required": ["latitude", "longitude"]
-                }
-            }
-        }
-    ]
+    ASSISTANT_ID = 'asst_h5ep6J3Mpvx29kO13XBTheKg'  # Jarvis Alpha v4 🤖
 
     def __init__(self, client: OpenAI):
         self.client = client
@@ -86,54 +41,242 @@ class JarvisAlphaService:
         :param request:
         :return:
         """
-        assistant = self.client.beta.assistants.retrieve(assistant_id=self.ASSISTANT_ID)
-        if not request.thread_id:
-            thread = self.client.beta.threads.create()
-        else:
-            thread = self.client.beta.threads.retrieve(thread_id=request.thread_id)
+        try:
+            assistant = self.client.beta.assistants.retrieve(assistant_id=self.ASSISTANT_ID)
+            if not request.thread_id:
+                thread = self.client.beta.threads.create()
+            else:
+                thread = self.client.beta.threads.retrieve(thread_id=request.thread_id)
 
-        message = self.client.beta.threads.messages.create(thread_id=thread.id,
-                                                           role="user",
-                                                           content=request.message)
-
-        run = self.client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-        )
-
-        while run.status == "queued" or run.status == "in_progress":
-            run = self.client.beta.threads.runs.retrieve(
+            message = self.client.beta.threads.messages.create(
                 thread_id=thread.id,
-                run_id=run.id,
+                role="user",
+                content=json.dumps({
+                    "message": request.message,
+                    "latitude": request.location.latitude,
+                    "longitude": request.location.longitude
+                })
             )
-            time.sleep(0.1)
 
-        if run.status == 'completed':
-            messages = self.client.beta.threads.messages.list(
-                thread_id=thread.id
+            run = self.client.beta.threads.runs.create_and_poll(
+                thread_id=thread.id,
+                assistant_id=assistant.id,
             )
-            print(messages)
-            return ResponseT(
-                code=Code.ok,
-                status=Status.ok,
-                data=messages.data[0].content[0].text.value
-            )
-        else:
-            messages = run.status
-            print(messages)
 
+            while run.status != 'completed':
+                tool_outputs = []
+                for tool in run.required_action.submit_tool_outputs.tool_calls:
+                    print(f"TO CALL {tool.function.name}")
+                    if tool.function.name == "get_nearby_places":
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_nearby_places(arguments.get("latitude"), arguments.get("longitude"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
+                    elif tool.function.name == "get_text_search":
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_text_search(arguments.get("textQuery"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
+                    elif tool.function.name == "get_geocoding":
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_geocoding(arguments.get("address"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
+                    elif tool.function.name == "get_reverse_geocoding":
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_reverse_geocoding(arguments.get("latlng"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
+                    elif tool.function.name == "get_autocomplete":
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_autocomplete(arguments.get("inputstr"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
+                    elif tool.function.name == "get_routes":
+                        print("I WAS HERE")
+                        arguments = json.loads(tool.function.arguments)
+                        call = self.get_routes(arguments.get("originLatLng"),
+                                               arguments.get("destinationLatLng"),
+                                               arguments.get("travelMode"))
+                        tool_outputs.append({
+                            "tool_call_id": tool.id,
+                            "output": json.dumps(call)
+                        })
 
-def get_places_from_text_search(query: str):
-    try:
-        response = requests.post('https://places.googleapis.com/v1/places:searchText',
-                                 headers={
-                                     'Content-Type': 'application/json',
-                                     'X-Goog-Api-Key': os.getenv('GOOGLE_MAPS_API_KEY'),
-                                     'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.priceLevel'
-                                 },
-                                 json={
-                                     "textQuery": query
-                                 })
+                print("TOOL OUTPUTS", tool_outputs)
+
+                if tool_outputs:
+                    try:
+                        run = self.client.beta.threads.runs.submit_tool_outputs_and_poll(
+                            thread_id=thread.id,
+                            run_id=run.id,
+                            tool_outputs=tool_outputs
+                        )
+                        print("Tool outputs submitted successfully.")
+                    except Exception as e:
+                        print("Failed to submit tool outputs:", e)
+                else:
+                    print("No tool outputs to submit.")
+
+            if run.status == 'completed':
+                messages = self.client.beta.threads.messages.list(thread_id=thread.id)
+                print(messages)
+                return ResponseT(
+                    code=Code.ok,
+                    status=Status.ok,
+                    data=json.loads(messages.data[0].content[0].text.value)
+                )
+            else:
+                print(run.required_action.submit_tool_outputs.tool_calls)
+
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def return_success(response: dict):
+        response["success"] = "true"
         return response
-    except Exception as e:
-        return str(e)
+
+    @staticmethod
+    def return_failed(e: str):
+        return {"success": "false",
+                "error": e}
+
+    def get_nearby_places(self, latitude: int, longitude: int):
+        FIELD_MASK = 'places.displayName,places.formattedAddress'
+        MAX_RESULT_COUNT = 3
+        RADIUS = 500
+        INCLUDED_TYPES = ["restaurant"]
+        try:
+            response = requests.post('https://places.googleapis.com/v1/places:searchNearby',
+                                     headers={
+                                         'Content-Type': 'application/json',
+                                         'X-Goog-Api-Key': os.getenv('GOOGLE_MAPS_API_KEY'),
+                                         'X-Goog-FieldMask': FIELD_MASK
+                                     },
+                                     json={"includedTypes": INCLUDED_TYPES,
+                                           "maxResultCount": MAX_RESULT_COUNT,
+                                           "locationRestriction": {
+                                               "circle": {
+                                                   "center": {
+                                                       "latitude": latitude,
+                                                       "longitude": longitude},
+                                                   "radius": RADIUS
+                                               }
+                                           }
+                                           })
+            return self.return_success(response.json())
+        except Exception as e:
+            print(e)
+            return self.return_failed(str(e))
+
+    def get_text_search(self, textQuery: str):
+        FIELD_MASK = 'places.displayName,places.formattedAddress'
+        try:
+            response = requests.post('https://places.googleapis.com/v1/places:searchText',
+                                     headers={
+                                         'Content-Type': 'application/json',
+                                         'X-Goog-Api-Key': os.getenv('GOOGLE_MAPS_API_KEY'),
+                                         'X-Goog-FieldMask': FIELD_MASK
+                                     },
+                                     json={
+                                         "textQuery": textQuery
+                                     })
+            return self.return_success(response.json())
+        except Exception as e:
+            print(e)
+            return self.return_failed(str(e))
+
+    def get_geocoding(self, address: str):
+        BASE_LINK = f"https://maps.googleapis.com/maps/api/geocode/json?"
+        ADDRESS = f"address={quote(address)}"
+        API_KEY = f"key={os.getenv("GOOGLE_MAPS_API_KEY")}"
+        try:
+            response = requests.post(f"{BASE_LINK}{ADDRESS}&{API_KEY}")
+            return self.return_success(response.json())
+        except Exception as e:
+            return self.return_failed(str(e))
+
+    def get_reverse_geocoding(self, latlng: str):
+        BASE_LINK = f"https://maps.googleapis.com/maps/api/geocode/json?"
+        LAT_LNG = f"latlng={latlng}"
+        API_KEY = f"key={os.getenv("GOOGLE_MAPS_API_KEY")}"
+        try:
+            response = requests.post(f"{BASE_LINK}{LAT_LNG}&{API_KEY}")
+            return self.return_success(response.json())
+        except Exception as e:
+            return self.return_failed(str(e))
+
+    def get_autocomplete(self, inputstr: str):
+        BASE_LINK = "https://places.googleapis.com/v1/places:autocomplete"
+        try:
+            response = requests.post(BASE_LINK,
+                                     headers={
+                                         'Content-Type': 'application/json',
+                                         'X-Goog-Api-Key': os.getenv('GOOGLE_MAPS_API_KEY'),
+                                     },
+                                     json={
+                                         "input": inputstr
+                                     })
+            return self.return_success(response.json())
+        except Exception as e:
+            return self.return_failed(str(e))
+
+    def get_routes(self, originLatLng: str, destinationLatLng: str, travelMode: Union[str, Literal[
+        "TWO_WHEELER",
+        "TRANSIT",
+        "DRIVE"
+    ]]):
+        BASE_LINK = "https://routes.googleapis.com/directions/v2:computeRoutes"
+        FIELD_MASK = 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+        origin = originLatLng.split(",")
+        originLat, originLng = float(origin[0]), float(origin[1])
+        destination = destinationLatLng.split(",")
+        destinationLat, destinationLng = float(destination[0]), float(destination[1])
+        try:
+            response = requests.post(BASE_LINK,
+                                     headers={
+                                         'Content-Type': 'application/json',
+                                         'X-Goog-Api-Key': os.getenv('GOOGLE_MAPS_API_KEY'),
+                                         'X-Goog-FieldMask': FIELD_MASK,
+                                     },
+                                     json={
+                                         "origin": {
+                                             "location": {
+                                                 "latLng": {
+                                                     "latitude": originLat,
+                                                     "longitude": originLng
+                                                 }
+                                             }
+                                         },
+                                         "destination": {
+                                             "location": {
+                                                 "latLng": {
+                                                     "latitude": destinationLat,
+                                                     "longitude": destinationLng
+                                                 }
+                                             }
+                                         },
+                                         "travelMode": travelMode,
+                                         # "computeAlternativeRoutes": False,
+                                         # "routeModifiers": {
+                                         #     "avoidTolls": False,
+                                         #     "avoidHighways": False,
+                                         #     "avoidFerries": False
+                                         # },
+                                         "languageCode": "en-US",
+                                         "units": "METRIC"
+                                     })
+            return self.return_success(response.json())
+        except Exception as e:
+            return self.return_failed(str(e))
